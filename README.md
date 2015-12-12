94a23edf3aed71bb06db0475b29068573b471650
Jessica Alessandro <JessicaAlessandro@Jessicas-MacBook-Pro.local>
2015-12-12 02:17:22 -0500

    installed dependencies and began front end css

---
+++ README.md
@@ -0,0 +1,16 @@
+MVP:
+	- Using the Wunderground API, a user enters a zip code and if the current temperature is above 60 degrees Fahrenheit, it will access the Yelp API and produce 8 nearby bars and restaurants at  random that have outdoor seating. If the temperature is below 60 degrees, the user will get a message stating that it's too cold to enjoy the outdoors. 
+
+APIs:
+	- Yelp
+	- Wunderground
+
+Bonuses: 
+	- Added User model and CRUD functionality.
+	- Users can bookmark bars/restaurants.
+	- Use Google Geolocation API, so the user's location is automatically tracked. 
+
+Schedule:
+E.O.D. 12/11 : Complete functionality for weather API
+E.O.D. 12/12 : Implement Yelp API
+E.O.D. 12/13 : Connect weather and Yelp APIs so the user only needs to input zipcode once. 
---
+++ server.js
@@ -0,0 +1,46 @@
+// =============
+// REQUIREMENTS
+// =============
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    nodeDebugger = require('node-debugger'),
+    morgan = require('morgan'),
+    md5 = require('md5'),
+    cookieParser = require('cookie-parser');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+// =============
+// MIDDLEWARE
+// =============
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+
+// =============
+// DATABASE
+// =============
+mongoose.connect('mongodb://localhost/finalproject', function(){
+    console.log('connected to database!')
+});
+
+// =============
+// MODELS
+// =============
+
+
+// =============
+// LISTENER
+// =============
+app.listen(port);
+
+// =============
+// ROUTES
+// =============
